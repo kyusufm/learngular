0fcefd0f88274f17ad862befe62d04c5a19abac8
Kyusuf Muhammad <kyusufm@gmail.com>
2015-01-26 15:29:39 +0700

    Display data dari database menggunakan angular,php, dan mysql

--- profilist/getuser.php
+++ profilist/getuser.php
@@ -4,14 +4,16 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $conn = new mysqli("localhost", "root", "", "learngular");
 
-$result = $conn->query("SELECT nama, gelar, detail FROM user");
+$result = $conn->query("SELECT * FROM user");
 
 $outp = "[";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "[") {$outp .= ",";}
     $outp .= '{"Name":"'  . $rs["nama"] . '",';
     $outp .= '"Gelar":"'   . $rs["gelar"]        . '",';
+    $outp .= '"Image":"'   . $rs["img"]        . '",';
     $outp .= '"Detail":"'. $rs["detail"]     . '"}'; 
+
 }
 $outp .="]";
 $conn->close();
--- profilist/index.php
+++ profilist/index.php
@@ -3,22 +3,39 @@
 <head>
 <!-- <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script> -->
 <script type="text/javascript" src="../js/angular.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="../js/bootstrap.min.js"></script>
 </head>
 <body>
 <div ng-app="" ng-controller="userController"> 
-
-<table>
+<h2> Get Data from MySQL database using $scope</h2>
+<table class="table">
   <tr ng-repeat="x in user">
     <td>{{ x.Name }}</td>
     <td>{{ x.Gelar }}</td>
     <td>{{ x.Detail }}</td>
   </tr>
 </table>
+<div ng-app="test" ng-controller="userController">
+<h3>other layout preview</h3>
+<div class="user row" ng-repeat="x in user">
+	<div class="col-md-3"><img ng-src="{{x.Image}}" class="img-responsive"></div>
+	<div class="col-md-9">
+		<p class="nama">{{x.Name}}</p>
+		<p class="gelar">{{ x.Gelar }}</p>
+		<p class="detail">{{ x.Detail }}</p>
+	</div>
+	<hr>
 </div>
-
 <script>
 function userController($scope,$http) {
-    var site = "http://localhost";
+    // var site = "http://localhost";
     var page = "getuser.php";
     $http.get(page)
     .success(function(response) {$scope.user = response;});
